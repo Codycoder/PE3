547fd44a68d69ef2232e2bc39ed513f72641a6fc
Codycoder <andykohler99@gmail.com>
2020-09-21 22:49:05 -0400

    First commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		
+				int num = 6;
+				List<int> primeFactors = new List<int>();
+
+			for (var i = 1; i < num; i++)
+			{
+				int factor = num / 2;
+				//primeFactors.Add(2);
+
+				if (factor > 3)
+				{
+					factor = num / 2;
+					primeFactors.Add(2);
+				}
+				else if (factor == 2)
+				{
+					primeFactors.Add(2);
+				}
+
+				else if (factor == 3)
+				{
+					primeFactors.Add(3);
+					break;
+				}
+				
+
+				foreach (var item in primeFactors)
+				{
+					Console.WriteLine(item);
+
+				}
+			}
+			
+		}
+    }
+}
